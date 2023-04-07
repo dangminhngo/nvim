92c764bcb105948639ff8d4d37e40a6973ec0df0
dangminhngo <dangminhngo.dev@gmail.com>
2023-04-08 01:09:42 +0700

    feat: replace vim-illuminate with mini.cursorword

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,26 @@
 return {
   {
+    -- dir = "/home/jaime/repos/proxima.nvim",
+    -- dev = true,
     "dangminhngo/proxima.nvim",
     lazy = true,
     opts = {
-      styles = {
-        attribute = "italic",
-        comment = "italic",
-      },
+      plugins = {
+        alpha = true,
+        dashboard = true,
+        diff = true,
+        gitsigns = true,
+        indent_blankline = true,
+        lazy = true,
+        mini = true,
+        neo_tree = true,
+        nvim_cmp = true,
+        nvim_notify = true,
+        nvim_ts_rainbow = true,
+        telescope = true,
+        trouble = true,
+        which_key = true,
+      }
     },
     init = function()
       vim.cmd([[colorscheme proxima]])
