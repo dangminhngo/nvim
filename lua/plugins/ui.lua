122a46aaf6827018705abd8e1b4d7c932c2f14d6
dangminhngo <dangminhngo.dev@gmail.com>
2023-04-08 01:17:26 +0700

    fix: remove `indent-blankline`

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -146,18 +146,6 @@ return {
     end,
   },
 
-  -- indent guides for Neovim
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      char = "▏",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
-    },
-  },
-
   -- active indent guide and indent text objects
   {
     "echasnovski/mini.indentscope",
