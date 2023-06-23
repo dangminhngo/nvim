b1cb1c67fbbb840e76cde49bb2f6c7bed978ef2b
dangminhngo <dangminhngo.dev@gmail.com>
2023-06-24 00:34:37 +0700

    feat(flash): add flash.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -370,4 +370,39 @@ return {
       { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
     },
   },
+
+  -- Enhanced search highlights
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          -- default options: exact mode, multi window, all directions, with a backdrop
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+    },
+  },
 }
