8fefee4ea9a6b51c9be5f791730ffc37daa66adb
dangminhngo <dangminhngo.dev@gmail.com>
2023-07-26 18:23:17 +0700

    fix: change colorscheme from github repo

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,8 @@
 return {
   {
-    dir = "~/repos/crux.nvim",
-    dev = true,
-    -- "dangminhngo/proxima.nvim",
-    -- lazy = true,
-    -- opts = {
-    -- },
+    -- dir = "~/repos/crux.nvim",
+    -- dev = true,
+    "dangminhngo/crux.nvim",
+    lazy = true,
   },
 }
